00c25427251a908f4907cfc801c6a86958a0b6da
Stephen Zerne <szerne@gmail.com>
2017-02-01 15:58:55 -0600

    update exclude

---
+++ dist/constants.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var CONSTANTS = exports.CONSTANTS = {
+    WEATHER_API_KEY: '6495f05bb22e737c',
+    NEWS_API_KEY: '16753354fab44e17869b85d2a030ac04'
+};
+//# sourceMappingURL=constants.js.map
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const CONSTANTS = {
+    WEATHER_API_KEY: '2e44fa814977dfa1',
+    NEWS_API_KEY: 'fabae52bfa17458cb91785b590f939fc'
+}
